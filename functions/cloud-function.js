f779959e8ba37f1a1d64903037a8d5eb466cd38c
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2024-09-11 13:14:42 +0530

    getBaseCollectorUrl

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -1,7 +1,20 @@
 export default function handler(req, res) {
     console.log("process.env",process.env)
     console.log("This is a console")
+    const withOutPath = getBaseCollectorUrl("https://dev-launch-api.csnonprod.com/telemetry");
+    const withPath = getBaseCollectorUrl("https://dev-launch-api.csnonprod.com/telemetry/v1/logs")
     res
     .status(200)
-    .json({ env: JSON.stringify(process.env) })
+    .json({ env: JSON.stringify(process.env), withOutPath, withPath })
 }
+
+function getBaseCollectorUrl(url) {
+    if (!url) return '';
+  
+    const logsPath = '/v1/logs';
+    if (url.endsWith(logsPath)) {
+      return url.replace(logsPath, '');
+    }
+  
+    return url.trim();
+  }
