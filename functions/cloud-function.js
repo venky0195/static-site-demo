bdd746a8df552e794987406cd8ede434b28bb351
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2024-09-11 12:35:43 +0530

    return response in cloud functions

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
     console.log("process.env",process.env)
     console.log("This is a console")
+    res
+    .status(200)
+    .json({ env: JSON.stringify(process.env) })
 }
