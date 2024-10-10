5cc207fa4fc05f338e7bdcad68139530be2017f5
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2024-10-10 15:48:04 +0530

    update edge

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,6 +1,5 @@
 export default function handler(request, context) {
-    console.log("Headers -> ", JSON.stringify(request.headers));
-    console.log("Stringified Headers -> ", JSON.stringify(request.headers));
-    console.log("x-forwarded for",request.headers.get('x-forwarded-for'));
+    console.log("Headers->",Object.fromEntries(request.headers));
+
     return fetch(request)
 }
