c5b5bd78fa7bc937e63c4538b89434a9c7ee5759
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2024-10-10 15:45:20 +0530

    add log for parsed header

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,5 +1,6 @@
 export default function handler(request, context) {
     console.log("Headers -> ", JSON.stringify(request.headers));
+    console.log("Stringified Headers -> ", JSON.stringify(request.headers));
     console.log("x-forwarded for",request.headers.get('x-forwarded-for'));
     return fetch(request)
 }
